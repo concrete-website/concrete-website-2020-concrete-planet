1150a59f2498465c1323238ecf6bbdc818bcd68e
anas <anas3506606@gmail.com>
2020-09-14 16:34:22 +0300

    Updates

---
+++ precache-manifest.47c4a9ca3d0b510e120d9a492c7143ac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa236401f0fe75b18992fe550b9992c4",
+    "url": "/concrete-website-2020-concrete-planet/index.html"
+  },
+  {
+    "revision": "de30ff1084b953f3627c",
+    "url": "/concrete-website-2020-concrete-planet/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "be38ab75e4daf9f1b47a",
+    "url": "/concrete-website-2020-concrete-planet/static/css/main.5058eb7f.chunk.css"
+  },
+  {
+    "revision": "de30ff1084b953f3627c",
+    "url": "/concrete-website-2020-concrete-planet/static/js/2.a418899f.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/concrete-website-2020-concrete-planet/static/js/2.a418899f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be38ab75e4daf9f1b47a",
+    "url": "/concrete-website-2020-concrete-planet/static/js/main.2e8e4dcf.chunk.js"
+  },
+  {
+    "revision": "be65966078f7bdcf36e5",
+    "url": "/concrete-website-2020-concrete-planet/static/js/runtime-main.f7521720.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/concrete-website-2020-concrete-planet/static/media/logo.5d5d9eef.svg"
+  }
+]);
